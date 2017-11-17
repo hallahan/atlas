f50d6f294a4933dc663c6511c8a1b03de0dbf696
Rodrick Megraw <rmegraw@apple.com>
2017-11-17 11:43:36 -0800

    Fix access to static predicate member in test.

--- src/test/java/org/openstreetmap/atlas/geography/atlas/ShardFileOverlapsPolygonTest.java
+++ src/test/java/org/openstreetmap/atlas/geography/atlas/ShardFileOverlapsPolygonTest.java
@@ -25,25 +25,25 @@ public class ShardFileOverlapsPolygonTest
     private static final Polygon POLYGON = Rectangle.forCorners(
             Location.forString("55.5868837,12.3541246"), Location.forString("55.752623,12.71942"));
 
-    private static final Predicate<Resource> predicate = new ShardFileOverlapsPolygon(SHARDING_TREE,
+    private static final Predicate<Resource> PREDICATE = new ShardFileOverlapsPolygon(SHARDING_TREE,
             POLYGON);
 
     @Test
     public void testFilenameFormat()
     {
         // variations on valid filename
-        Assert.assertTrue(this.predicate.test(new File("/some/path/DNK_11-1095-641.atlas")));
-        Assert.assertTrue(this.predicate.test(new File("/some/path/DNK_11-1095-641")));
+        Assert.assertTrue(PREDICATE.test(new File("/some/path/DNK_11-1095-641.atlas")));
+        Assert.assertTrue(PREDICATE.test(new File("/some/path/DNK_11-1095-641")));
 
         // some filenames that aren't formatted properly
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_11_1095_641.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_11_1095_641.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/11-1095-641.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_11-1095-641.atl.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_110-1095-641.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_11-1095-641.atlas.gzip")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/foo")));
-        Assert.assertFalse(this.predicate.test(new File("")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_11_1095_641.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_11_1095_641.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/11-1095-641.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_11-1095-641.atl.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_110-1095-641.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_11-1095-641.atlas.gzip")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/foo")));
+        Assert.assertFalse(PREDICATE.test(new File("")));
     }
 
     @Test
@@ -51,33 +51,33 @@ public class ShardFileOverlapsPolygonTest
     {
         // resources that are not files
         Assert.assertFalse(
-                this.predicate.test(new StringResource("/some/path/DNK_11-1095-641.atlas.gz")));
+                PREDICATE.test(new StringResource("/some/path/DNK_11-1095-641.atlas.gz")));
     }
 
     @Test
     public void testShardsInBounds()
     {
         // shards that should overlap a polygon in Copehagen
-        Assert.assertTrue(this.predicate.test(new File("/some/path/DNK_11-1095-641.atlas.gz")));
-        Assert.assertTrue(this.predicate.test(new File("/some/path/DNK_11-1094-641.atlas.gz")));
-        Assert.assertTrue(this.predicate.test(new File("/some/path/DNK_11-1095-640.atlas.gz")));
-        Assert.assertTrue(this.predicate.test(new File("/some/path/DNK_11-1094-640.atlas.gz")));
-        Assert.assertTrue(this.predicate.test(new File("/some/path/DNK_10-548-320.atlas.gz")));
+        Assert.assertTrue(PREDICATE.test(new File("/some/path/DNK_11-1095-641.atlas.gz")));
+        Assert.assertTrue(PREDICATE.test(new File("/some/path/DNK_11-1094-641.atlas.gz")));
+        Assert.assertTrue(PREDICATE.test(new File("/some/path/DNK_11-1095-640.atlas.gz")));
+        Assert.assertTrue(PREDICATE.test(new File("/some/path/DNK_11-1094-640.atlas.gz")));
+        Assert.assertTrue(PREDICATE.test(new File("/some/path/DNK_10-548-320.atlas.gz")));
     }
 
     @Test
     public void testShardsOutOfBounds()
     {
         // shards that should not overlap a polygon in Copenhagen
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_10-546-319.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_10-547-319.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_10-548-319.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_10-546-320.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_10-546-321.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_10-547-321.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_10-548-321.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_10-546-321.atlas.gz")));
-        Assert.assertFalse(this.predicate.test(new File("/some/path/DNK_10-548-321.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_10-546-319.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_10-547-319.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_10-548-319.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_10-546-320.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_10-546-321.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_10-547-321.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_10-548-321.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_10-546-321.atlas.gz")));
+        Assert.assertFalse(PREDICATE.test(new File("/some/path/DNK_10-548-321.atlas.gz")));
     }
 
 }
