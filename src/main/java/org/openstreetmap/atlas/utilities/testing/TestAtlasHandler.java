3397b32b40ecd8959e23ccb05b4169073da36563
Mike Gostintsev <mgostintsev@apple.com>
2018-01-22 09:39:26 -0800

    Changes from Public Dev to Apple (#58)
    * Add debug statement when new shard is not within max bounds (#60)
    
    Adding a debug statement in getAtlasFetcher for when the new shard to be fetched is outside the maximumBounds.
    
    Before:
    ```
    2018-01-12 14:32:55 INFO  DynamicAtlas:524 - DynamicAtlas([7-80-52]): Loading new shard [Z]-[X]-[Y] found no new Atlas.
    ```
    
    After:
    ```
    2018-01-12 14:32:55 DEBUG DynamicAtlasPolicy:106 - Skipping atlasFetcher for [Z]-[X]-[Y] because shard bounds are outside the policy's maximumBounds
    2018-01-12 14:32:55 INFO  DynamicAtlas:524 - DynamicAtlas([7-80-52]): Loading new shard [Z]-[X]-[Y] found no new Atlas.
    ```
    
    * Thread safety on the AtlasFeatureCountsSubCommand (#59)
    
    * Thread-safety on the AtlasFeatureCountsSubCommand
    
    If `AtlasReader` is used with multiple shards it will use multiple threads in the `handle` method, meaning we need to be careful about how we store values in the non-concurrent hashmap.
    
    I also wrapped the `FileOutputStream` in a `BufferedOutputStream` for smoother I/O performance.
    
    * Added parallel flag to AtlasReader
    
    This lets us optionally run all of the incoming atlases in parallel, improving runtime performance.
    
    * Added support for setting the ISO country code
    
    Now we can optionally set the iso country code of a `TestAtlas` in case test cases need to know that information based on the metadata and not from individual `AtlasEntity` tags
    
    * Fixed AtlasSplitter test case
    
    The original code called the subcommand class’s `execute` method directly.  This is wrong, because it bypasses the machinery of `Command` to auto-populate `Flags` that haven’t been set.
    
    * Added test case for the AtlasFeatureCounts
    
    This verifies that parallel passes through `AtlasFeatureCountsSubCommand` creates the correct results.
    
    * Updated per MikeGost’s comments
    
    Switched from NIO to Atlas `File` APIs and added an extra assertion in the test case
    
    * Replaced Guava Iterables with Atlas Iterables
    
    * Route Reversal Functionality (#61)
    
    * Route reverse functionality
    
    * satisfying stylecheck for rule class
    
    * Fixing intersection methods, adding test and test data (#62)
    
    * Supporting loading from OSM xml without JOSM parsing (#64)

--- src/main/java/org/openstreetmap/atlas/utilities/testing/TestAtlasHandler.java
+++ src/main/java/org/openstreetmap/atlas/utilities/testing/TestAtlasHandler.java
@@ -16,6 +16,7 @@ import org.openstreetmap.atlas.geography.Longitude;
 import org.openstreetmap.atlas.geography.PolyLine;
 import org.openstreetmap.atlas.geography.Polygon;
 import org.openstreetmap.atlas.geography.atlas.Atlas;
+import org.openstreetmap.atlas.geography.atlas.AtlasMetaData;
 import org.openstreetmap.atlas.geography.atlas.builder.AtlasSize;
 import org.openstreetmap.atlas.geography.atlas.builder.RelationBean;
 import org.openstreetmap.atlas.geography.atlas.builder.text.TextAtlasBuilder;
@@ -32,6 +33,7 @@ import org.openstreetmap.atlas.tags.BuildingPartTag;
 import org.openstreetmap.atlas.tags.BuildingTag;
 import org.openstreetmap.atlas.tags.RelationTypeTag;
 import org.openstreetmap.atlas.tags.annotations.validation.Validators;
+import org.openstreetmap.atlas.utilities.collections.Maps;
 import org.openstreetmap.atlas.utilities.collections.StringList;
 import org.openstreetmap.atlas.utilities.testing.TestAtlas.Area;
 import org.openstreetmap.atlas.utilities.testing.TestAtlas.Area.Known;
@@ -113,7 +115,20 @@ public class TestAtlasHandler implements FieldHandler
         {
             try
             {
-                loadFromJosmOsmResource(field, rule, context, testAtlas.loadFromJosmOsmResource());
+                loadFromJosmOsmResource(field, rule, context, testAtlas.loadFromJosmOsmResource(),
+                        true);
+            }
+            catch (final Throwable e)
+            {
+                throw new CoreException("Error creating field {}", field, e);
+            }
+        }
+        else if (StringUtils.isNotEmpty(testAtlas.loadFromOsmResource()))
+        {
+            try
+            {
+                loadFromJosmOsmResource(field, rule, context, testAtlas.loadFromOsmResource(),
+                        false);
             }
             catch (final Throwable e)
             {
@@ -204,8 +219,18 @@ public class TestAtlasHandler implements FieldHandler
     {
 
         final PackedAtlasBuilder builder = new PackedAtlasBuilder();
-        builder.setSizeEstimates(convertSizeEstimates(testAtlas.size()));
-
+        final AtlasSize size = convertSizeEstimates(testAtlas.size());
+        final String iso = testAtlas.iso();
+        if (!iso.equals(TestAtlas.UNKNOWN_ISO_COUNTRY))
+        {
+            final AtlasMetaData metaData = new AtlasMetaData(size, true, null, null, iso, null,
+                    Maps.hashMap());
+            builder.withMetaData(metaData);
+        }
+        else
+        {
+            builder.setSizeEstimates(size);
+        }
         handle(builder, testAtlas.nodes());
         handle(builder, testAtlas.edges());
         handle(builder, testAtlas.areas());
@@ -336,7 +361,7 @@ public class TestAtlasHandler implements FieldHandler
     }
 
     private void loadFromJosmOsmResource(final Field field, final CoreTestRule rule,
-            final CreationContext context, final String resourcePath)
+            final CreationContext context, final String resourcePath, final boolean josmFormat)
     {
         final String packageName = rule.getClass().getPackage().getName().replaceAll("\\.", "/");
         final String completeName = String.format("%s/%s", packageName, resourcePath);
@@ -350,10 +375,19 @@ public class TestAtlasHandler implements FieldHandler
         });
         try
         {
-            final StringResource osmFile = new StringResource();
-            new OsmFileParser().update(resource, osmFile);
             final ByteArrayResource pbfFile = new ByteArrayResource();
-            new OsmFileToPbf().update(osmFile, pbfFile);
+            if (josmFormat)
+            {
+                // If the XML file is in JOSM format, fix it to look like an OSM file
+                final StringResource osmFile = new StringResource();
+                new OsmFileParser().update(resource, osmFile);
+                new OsmFileToPbf().update(osmFile, pbfFile);
+            }
+            else
+            {
+                new OsmFileToPbf().update(resource, pbfFile);
+            }
+
             field.set(rule, new OsmPbfLoader(pbfFile, AtlasLoadingOption.withNoFilter()).read());
         }
         catch (IllegalArgumentException | IllegalAccessException e)
